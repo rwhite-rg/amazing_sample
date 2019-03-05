bb9b14d1edf2abcf3e08c7a3038e996ec0ebfb59
Richard White <heregrima@gmail.com>
2019-03-05 09:27:47 +0000

    Fixed: 1)  Added files that I forgot to add with JWT config 2)  Added controllers for User, Home, and Data (to be expanded later) 3)  Added JWT classes to Kernel for routes 4)  Added JwtMiddleware class to handle incoming requests 5)  Changed web routes over to api routes (...but I forgot to add the web routes file, so I'll do that next commit because I'm lazy)

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', 'UserController@getAuthenticatedUser');
 });
